176267ccc2c05f1247dd0be26c5b17b88ca5dc2f
karan mandhare <2002karanmandhare@gmail.com>
2025-01-26 14:05:59 +0530

    first

--- src/main/java/com/edigest/journelApp/respository/JournalEntryRepository.java
+++ src/main/java/com/edigest/journelApp/respository/JournalEntryRepository.java
@@ -1,10 +1,14 @@
 package com.edigest.journelApp.respository;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.edigest.journelApp.entity.JournalEntry;
 
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+	Optional<JournalEntry> save(Optional<JournalEntry> myEntry);
 
 }
